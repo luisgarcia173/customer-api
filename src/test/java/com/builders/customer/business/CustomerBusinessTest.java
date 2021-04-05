2b5004117b7283b10d61a6bfb6b3ee13fbe4fcd4
Luis Garcia <luis.garcia@valecard.com.br>
2021-04-05 15:49:09 -0300

    Removido imports desnecessarios

--- src/test/java/com/builders/customer/business/CustomerBusinessTest.java
+++ src/test/java/com/builders/customer/business/CustomerBusinessTest.java
@@ -1,49 +1,29 @@
 package com.builders.customer.business;
 
-import br.com.valecard.simpleapi.business.dto.VehicleDto;
-import br.com.valecard.simpleapi.business.impl.VehicleBusinessImpl;
-import br.com.valecard.simpleapi.exceptions.DuplicateAttemptException;
-import br.com.valecard.simpleapi.exceptions.MandatoryParametersNotFoundException;
-import br.com.valecard.simpleapi.exceptions.VehicleNotFoundException;
-import br.com.valecard.simpleapi.repositories.VehicleRepository;
-import br.com.valecard.simpleapi.repositories.entities.Vehicle;
-import br.com.valecard.simpleapi.repositories.enums.VehicleStatusEnum;
 import com.builders.customer.business.impl.CustomerBusinessImpl;
 import com.builders.customer.exceptions.CustomerNotFoundException;
 import com.builders.customer.exceptions.ExistingCustomerException;
 import com.builders.customer.repositories.CustomerRepository;
 import com.builders.customer.repositories.entities.Customer;
 import com.builders.customer.repositories.enums.DocumentTypeEnum;
-import com.builders.customer.resources.dtos.AddressDto;
 import com.builders.customer.resources.dtos.CustomerDto;
 import com.builders.customer.resources.dtos.DocumentDto;
-import com.builders.customer.resources.dtos.PhoneDto;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith( SpringExtension.class )
